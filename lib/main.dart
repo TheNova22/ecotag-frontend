25c663cc4b903aa36a4637762037fead3cdc98a1
Prash2002 <1707prash@gmail.com>
2022-08-18 02:43:50 +0530

    simple UI login

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:sih_frontend/screens/api_test.dart';
-import 'package:sih_frontend/screens/login_screen.dart';
+import 'package:sih_frontend/screens/login/login_screen.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/services.dart';
 import 'package:sih_frontend/utils/api_functions.dart';
